9cd8b76af1e6827ac5b28957f725f1fd49e04fe8
johannes <wichertj@students.uni-marburg.de>
2026-01-11 16:12:53 +0100

    Processes can now message master process with individual medians. Small outline for The rest of the kd-Tree algorithm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,63 +1,49 @@
-import mpi.*;
+import mpi.MPI;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+static private ArrayList<Point> data;
+static private int dimensions;
 
-public class Main {
-
-    static private ArrayList<double[]> data;
-    static private int dimensions;
-
-    /**
-     * Reads the specified csv file and sets the data and dimensions fields
-     * @param filename
-     * @throws IOException
-     */
-    static private void readData(String filename) throws IOException {
-        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
-            data = (ArrayList<double[]>) stream.map(s -> s.split(","))
-                    .map(arr -> {
-                        double[] Doubles = new double[arr.length];
-                        int i = 0;
-                        for (String s : arr) {
-                            Doubles[i] = Double.parseDouble(s.trim());
-                            i++;
+/**
+ * Reads the specified csv file and sets the data and dimensions fields
+ * @param filename path to the used file
+ * @throws IOException just an ioexception
+ */
+static private void readData(String filename) throws IOException {
+    try (Stream<String> stream = Files.lines(Paths.get(filename))) {
+        data = (ArrayList<Point>) stream.map(s -> s.split(","))
+                .map(arr -> {
+                            double[] Doubles = new double[arr.length];
+                            int i = 0;
+                            for (String s : arr) {
+                                Doubles[i] = Double.parseDouble(s.trim());
+                                i++;
+                            }
+                            return new Point(Doubles);
                         }
-                        return Doubles;
-                    }
-            ).collect(Collectors.toList());
-        }
-        dimensions = data.get(0).length;
+                ).collect(Collectors.toList());
+    }
+    dimensions = data.get(0).dimensions;
+}
+
+void main(String[] args) throws Exception {
+    readData("src/main/resources/datasets/densired_2_truncated.csv");
+    MPI.Init(args);
+
+    int numOfProcesses = MPI.COMM_WORLD.Size();
+    int rank = MPI.COMM_WORLD.Rank();
+    int blockSize;
+    int dataSize = data.size();
+    blockSize = dataSize / numOfProcesses;
+    if (dataSize % numOfProcesses != 0) {
+        blockSize++; // one more if data size isn't divisible by the number of processes
     }
 
-    static private void makeDataBlocks() {
+    Process process = new Process(
+            new ArrayList<>(data.subList(rank * blockSize, Math.min((rank + 1) * blockSize, dataSize)))
+    );
 
-    }
+    process.calculateIndividualMedian(1);
+    IO.println("Hello from rank " + process.rank + " of " + MPI.COMM_WORLD.Size() + " with data: " + process.points.get(0).coords[0] + ", " + process.points.get(0).coords[1] + " and " + process.points.size() + " points and x-median " + process.median);
 
-    public static void main(String[] args) throws Exception {
-        readData("src/main/resources/datasets/densired_2_truncated.csv");
-        MPI.Init(args);
-
-        int numOfProcesses = MPI.COMM_WORLD.Size();
-        int rank = MPI.COMM_WORLD.Rank();
-        int blockSize;
-        int dataSize = data.size();
-        blockSize = dataSize / numOfProcesses;
-        if (dataSize%numOfProcesses != 0) {
-            blockSize++; // one more if data size isn't divisible by the number of processes
-        }
-
-        Process process = new Process(
-                new ArrayList<> (data.subList(rank * blockSize, Math.min((rank+1) * blockSize, dataSize)))
-        );
-
-        System.out.println("Hello from rank " + process.rank + " of " + MPI.COMM_WORLD.Size() + " with data: " +  process.points.get(0)[0] + ", " + process.points.get(0)[1] + " and " + process.points.size() + " points.");
-
-        MPI.Finalize();
-    }
+    MPI.Finalize();
 }
--- src/main/java/Process.java
+++ src/main/java/Process.java
@@ -1,34 +1,86 @@
+import mpi.Group;
+import mpi.Intracomm;
 import mpi.MPI;
+import mpi.Request;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
 
 public class Process {
-    ArrayList<double[]> points;
-    int rank;
-    int rootRank; // rank of the root process
-    int partner; // rank of the partner process to send to
-    boolean highPartner; // whether to gather high point data or low point data
-    int processesInNode; // number of processes in the current node of the kd-tree
 
-    public Process(ArrayList<double[]> data) {
+    // tags for message passing
+    private final int INDIVIDUALMEDIAN = 0;
+    private final int GROUPMEDIAN = 1;
+    private final int POINTEXCHANGE = 2;
+
+    ArrayList<Point> points;
+    int rank; // rank in the current group
+    int processesInGroup; // number of processes in the current node of the kd-tree
+    Group group;
+    Intracomm communicator;
+    double median;
+
+    public Process(ArrayList<Point> data) {
         points = data;
         rank = MPI.COMM_WORLD.Rank();
-        rootRank = 0; // initially the first process, changes during partitioning
-        processesInNode = MPI.COMM_WORLD.Size();
-
+        processesInGroup = MPI.COMM_WORLD.Size();
+        group = MPI.COMM_WORLD.Group();
     }
 
-    public double sortAndCalculateMedian(int dimension) {
-        int column = dimension-1;
-        points.sort(Comparator.comparingDouble(arr -> arr[column]));
+    public void calculateIndividualMedian(int dimension) {
+        int column = dimension - 1;
+        points.sort(Comparator.comparingDouble(point -> point.coords[column]));
         int size = points.size();
-        if (points.size()%2 == 1) {
-            return points.get((size-1)/2)[column];
+        if (points.size() % 2 == 1) {
+            median = points.get((size - 1) / 2).coords[column];
+            return;
         }
-        return (points.get((size-2)/2)[column] + points.get((size/2))[column]) / 2;
+        median = (points.get((size - 2) / 2).coords[column] + points.get((size / 2)).coords[column]) / 2;
     }
+
+    public void sendIndividualMedian() {
+        double[] message = new double[]{median};
+        communicator.Send(
+                message, // message buffer to send
+                0, // offset of message start
+                1, // number of items to send
+                MPI.DOUBLE, // data type to send
+                0, // receiving rank (group master)
+                INDIVIDUALMEDIAN // tag
+        );
+    }
+
+    public void findGlobalMedian() {
+        RankValuePair[] allMedians = new RankValuePair[group.Size()];
+        allMedians[0] = new RankValuePair(this.rank, this.median);
+
+        Request[] requests = new Request[group.Size()-1];
+        double[][] recieveBuffers = new double[group.Size() - 1][1];
+
+        for (int i = 0; i < group.Size()-1; i++) {
+            requests[i] = communicator.Irecv( // non-blocking so messages don't have to arrive in order of rank
+                    recieveBuffers[i], // recieve buffer
+                    0, // offset
+                    1, // number of recieved message items
+                    MPI.DOUBLE, // data type
+                    i, // from process with specified rank
+                    INDIVIDUALMEDIAN // tag
+            );
+        }
+        Request.Waitall(requests); // wait for messages to arrive
+        for (int i = 0; i < recieveBuffers.length; i++) {
+            allMedians[i+1] = new RankValuePair(i+1, recieveBuffers[i][0]);
+        }
+
+        Arrays.sort(allMedians); // sorting to easily find median and also easily find send/recieve partners that have little data to send
+
+        // TODO
+    }
+
+    // TODO: Get Median, send Median and partner to all processes, define new groups and send message to update.
+    //  Also get an iteration counter and implement a workflow of functions.
+    //  New Masters should be notified to continue. The first master should be started from the main file.
+    //  Processes also need to exchange points
+
 }
