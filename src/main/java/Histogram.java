50f2749de38672b947e625b109b82d616f5d2266
johannes <wichertj@students.uni-marburg.de>
2026-01-27 12:09:21 +0100

    Histogram for Sliding-Window median calculation

---
+++ src/main/java/Histogram.java
@@ -0,0 +1,121 @@
+import mpi.Intracomm;
+import mpi.MPI;
+
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class Histogram {
+    private final NavigableMap<Double, Integer> hist = new TreeMap<>();
+    private double min;
+    private double max;
+
+    public Histogram(int dimension, Point[] points) {
+        min = points[0].coords[dimension];
+        max = min;
+        for (Point point : points) {
+            double key = point.coords[dimension];
+            int count = hist.getOrDefault(key, 0);
+            count++;
+            hist.put(key, count);
+            if (key > max) {
+                max = key;
+            } else if (key < min) {
+                min = key;
+            }
+        }
+    }
+
+    public void send(int dest, int tag, Intracomm comm) {
+
+        int binNumber = hist.size();
+
+        int[] header = {binNumber};
+        comm.Send(header, 0, 1, MPI.INT, dest, tag);
+
+        double[] keys = new double[binNumber];
+        int[] counts = new int[binNumber];
+        int i = 0;
+        for (Map.Entry<Double, Integer> e : hist.entrySet()) {
+            keys[0] = e.getKey();
+            counts[0] = e.getValue();
+            i++;
+        }
+        comm.Send(keys, 0, binNumber, MPI.DOUBLE, dest, tag);
+        comm.Send(counts, 0, binNumber, MPI.INT, dest, tag);
+    }
+
+    public void receiveAndMerge(int src, int tag, Intracomm comm) {
+
+        int[] header = new int[1];
+        comm.Recv(header, 0, 1, MPI.INT, src, tag);
+        int binNumber = header[0];
+
+        double[] keys = new double[binNumber];
+        int[] counts = new int[binNumber];
+        comm.Recv(keys, 0, binNumber, MPI.DOUBLE, src, tag);
+        comm.Recv(counts, 0, binNumber, MPI.INT, src, tag);
+
+        for (int i = 0; i < binNumber; i++) {
+            hist.put(keys[i], hist.getOrDefault(keys[i], 0) + counts[i]);
+        }
+    }
+
+    public double determineSlidingWindowMedian(double epsilon) {
+
+        //turn histogram into arrays for efficient sorting
+        double[] keys = hist.keySet().stream()
+                .mapToDouble(Double::doubleValue)
+                .toArray();
+        int[] counts = hist.values().stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+
+        int leftIdx = 0;
+        int rightIdx = 0;
+        int n = hist.size();
+
+        //prefix-sum
+        int[] prefix = new int[n + 1];
+        for (int i = 0; i < n; i++)
+            prefix[i + 1] = prefix[i] + counts[i];
+
+        int total = prefix[n];
+
+        double bestSplit = keys[0];
+        int bestImbalance = Integer.MAX_VALUE;
+
+        for (int splitIdx = 0; splitIdx < n; splitIdx++) {
+
+            double split = keys[splitIdx];
+
+            while (rightIdx < n && keys[rightIdx] <= split + epsilon)
+                rightIdx++;
+
+            while (leftIdx < n && keys[leftIdx] < split - epsilon)
+                leftIdx++;
+
+            int leftLoad = prefix[rightIdx];
+            int rightLoad = total - prefix[leftIdx];
+
+            int imbalance = Math.abs(leftLoad - rightLoad);
+
+            if (imbalance < bestImbalance) {
+                bestImbalance = imbalance;
+                bestSplit = split;
+            } else if (imbalance > bestImbalance) {
+                return bestSplit;
+            }
+        }
+        return bestSplit;
+    }
+
+    public double getMin() {
+        return min;
+    }
+
+    public double getMax() {
+        return max;
+    }
+}
